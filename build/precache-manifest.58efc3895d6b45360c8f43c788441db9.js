b83155830d99d1fa0edf47038f3feae6fa8f1f65
narkhedek3 <narkhedek3@gmail.com>
2020-05-10 23:06:38 +0530

    ok

---
+++ build/precache-manifest.58efc3895d6b45360c8f43c788441db9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919aeb78dd04b0332314dda5c24e9d69",
+    "url": "/React-app-rental-project/index.html"
+  },
+  {
+    "revision": "3777fe6008658fcdb487",
+    "url": "/React-app-rental-project/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c168743a07d706a0ce82",
+    "url": "/React-app-rental-project/static/css/main.2adb775a.chunk.css"
+  },
+  {
+    "revision": "3777fe6008658fcdb487",
+    "url": "/React-app-rental-project/static/js/2.991bc1dc.chunk.js"
+  },
+  {
+    "revision": "0e3576b43aafcb7c43f09ebe6e20b721",
+    "url": "/React-app-rental-project/static/js/2.991bc1dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c168743a07d706a0ce82",
+    "url": "/React-app-rental-project/static/js/main.a2ccaf1c.chunk.js"
+  },
+  {
+    "revision": "348547db1dfbd3bf4c6f",
+    "url": "/React-app-rental-project/static/js/runtime-main.200bf251.js"
+  }
+]);
